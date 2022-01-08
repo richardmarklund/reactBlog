d622557a4e7e5a35e56fa66e7c188507a580f443
Richard Marklund <richard.marklund@purplescout.se>
2022-01-08 07:23:50 +0100

    add new post funtionality

--- src/App.js
+++ src/App.js
@@ -4,43 +4,94 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Container } from "@mui/material";
+import { Container, TextField, Button, Box } from "@mui/material";
 import { Grid } from "@mui/material";
 import Post from "./post";
+import { getBlogPosts, addBlogPost } from "./storage";
+import { useState } from "react";
 import moment from "moment";
 
+function TextInputComponent({ handleClick }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-let blogContent = [{
-  date: moment().format("yyyy-MM-DD"),
-  title: "test Title",
-  content: "wtf content"
-},
-{
-  date: moment().format("yyyy-MM-DD"),
-  title: "test Title 2",
-  content: "wtf content 2"
-}]
+  return (
+    <Box>
+      <Box>
+        <Container>
+          <TextField
+            label="Title"
+            multiline
+            rows={1}
+            value={title}
+            onInput={(e) => setTitle(e.target.value)}
+          />
+        </Container>
+      </Box>
+      <Box pt={3}>
+        <Container>
+          <TextField
+            label="Content"
+            multiline
+            rows={10}
+            value={content}
+            onInput={(e) => setContent(e.target.value)}
+          />
+        </Container>
+        <Box />
+        <Box></Box>
+        <Container>
+          <Button
+            variant="text"
+            onClick={() =>
+              handleClick({
+                title: title,
+                content: content,
+                date: moment().format("YYYY-MM-DD"),
+              })
+            }
+          >
+            Save
+          </Button>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
+function BlogPostsComponent({ posts }) {
+  return (
+    <Box>
+      <Container>
+        <Typography variant="overline">
+          <Grid>{posts.map((post) => Post(post))}</Grid>
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
 
 function App() {
+  const [posts, setPosts] = useState(getBlogPosts());
+  const handleClick = (post) => {
+    setPosts([post, ...posts]);
+    console.log(post)
+    addBlogPost(post);
+  };
+
   return (
     <div>
       <CssBaseline />
-      <Grid container item justify="center" align="center">
+      <Grid container justifyContent="center" align="center">
         <Container>
-          <Typography variant="h1" justify="center">
-            Blog
-          </Typography>
+          <Typography variant="h1">Blog</Typography>
         </Container>
         <Container>
           <Typography variant="overline">
             The personal blog of Richard Marklund
           </Typography>
         </Container>
-        <Container>
-          <Typography variant="overline">
-          <Grid>{ blogContent.map(post =>  Post(post))}</Grid>
-          </Typography>
-        </Container>
+        <BlogPostsComponent posts={posts} />
+        <TextInputComponent handleClick={handleClick} />
       </Grid>
     </div>
   );
--- src/post.js
+++ src/post.js
@@ -1,25 +1,26 @@
 import Typography from "@mui/material/Typography";
-import { Container } from "@mui/material";
-
-
-
+import { Container, Box, Divider } from "@mui/material";
 
 function Post(props) {
-  const post  = props;
+  const post = props;
 
   return (
-    <Container>
-      <Typography component="h2" variant="h5">
-        {post.title}
-      </Typography>
-      <Typography variant="subtitle1" color="text.secondary">
-        {post.date.toString()}
-      </Typography>
-      <Typography variant="subtitle1" paragraph>
-        {post.content}
-      </Typography>
-
-    </Container>
+      <Box pt={3} style={{ whiteSpace: 'pre-wrap' }}>
+      <Divider ligtht="true" variant="middle" />
+      <Box pt={3}>
+        <Container>
+          <Typography component="h2" variant="h5">
+            {post.title}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            {post.date.toString()}
+          </Typography>
+          <Typography>
+            {post.content}
+          </Typography>
+        </Container>
+      </Box>
+    </Box>
   );
 }
 
