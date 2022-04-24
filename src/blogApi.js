a218e7a2c6cb04ec78513b667f994ebb7e445d6b
Richard Marklund <richard.marklund@gmail.com>
2022-04-24 07:30:37 +0000

    change back the url to backend api
    Created with Sturdy

--- src/blogApi.js
+++ src/blogApi.js
@@ -2,7 +2,7 @@ import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
 
-const url = 'http://localhost:3001'
+const url = 'https://backend.marklund.io'
 
 export const loginUser = async (username, password) => {
   return await fetch(`${url}/login`, {
